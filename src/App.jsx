d3bec439f57203809378abbb5dcca586ecd796f8
hannanaamir08 <aamirhannansde@gmail.com>
2023-10-30 20:08:13 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Header from './components/Header'
+import Body from './components/Body'
+import Footer from './components/Footer.jsx'
+import './index.css'
+
+const App = () => {
+  return (
+    <>
+    <Header/>
+    <Body/>
+    <Footer/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Header.jsx
@@ -0,0 +1,35 @@
+// const Title = ()=>{
+//   <a href="/">
+//     <img className="logo"      
+//     src="https://yt3.ggpht.com/ytc/AMLnZu_EC-ECXAxRAixWGEfMsE1rdSoetBHyxmLNdtCB=s900-c-k-c0x00ffffff-no-rj"
+//     alt="logo" 
+//     />
+//   </a>
+// };
+
+
+const Header = () => {
+  return (
+      <div className="header">
+        <div>
+          
+          <a href="/">
+            <img className="logo"      
+            src="/images/logo.jpg"
+            alt="logo" />
+          </a>
+
+        </div>
+        <div className="nav-items">
+          <ul>
+            <li>Home</li>
+            <li>About</li>
+            <li>Contact</li>
+            <li>Cart</li>
+          </ul>
+        </div>
+      </div>
+  )
+};
+
+export default Header;
